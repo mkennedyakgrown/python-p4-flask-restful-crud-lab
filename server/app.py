19b7b8cede860d3b7f253f372f300af28c263029
Matthew Kennedy <mkennedy.akgrown@gmail.com>
2024-04-11 14:17:45 -0500

    Completed Lab

--- server/app.py
+++ server/app.py
@@ -3,10 +3,12 @@
 from flask import Flask, jsonify, request, make_response
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
+from flask_cors import CORS
 
 from models import db, Plant
 
 app = Flask(__name__)
+CORS(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///plants.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
@@ -44,8 +46,27 @@ api.add_resource(Plants, '/plants')
 class PlantByID(Resource):
 
     def get(self, id):
-        plant = Plant.query.filter_by(id=id).first().to_dict()
+        plant = Plant.query.filter_by(id=id).first()
+        plant = plant.to_dict()
         return make_response(jsonify(plant), 200)
+    
+    def patch(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        request_json = request.get_json()
+        
+        for attr in request_json.keys():
+            setattr(plant, attr, request_json[attr])
+
+        db.session.add(plant)
+        db.session.commit()
+        return make_response(plant.to_dict(), 200)
+    
+    def delete(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        db.session.delete(plant)
+        db.session.commit()
+
+        return make_response("", 204)
 
 
 api.add_resource(PlantByID, '/plants/<int:id>')
